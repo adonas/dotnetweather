8d7cdc9eeba95096b1dde5b5603e7f1c633a11b1
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-06-17 10:29:12 -0600

    ..

--- SkyCast/SkayCast/Services/WeatherService.cs
+++ SkyCast/SkayCast/Services/WeatherService.cs
@@ -12,7 +12,7 @@ public class WeatherService
     public WeatherService(HttpClient http, IConfiguration config)
     {
         _http = http;
-        _key = config["OPENWEATHER_KEY"] ?? "";
+        _key = config["OPENWEATHER_KEY"] ?? "";        
     }
     public async Task<WeatherDto> GetByCityAsync(string city)
     {
