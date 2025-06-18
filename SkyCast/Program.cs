ef829e2be893298addb9905eacaf661cccbd62d0
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-06-18 17:57:32 -0600

    Corrección en config openweahther

--- SkyCast/Program.cs
+++ SkyCast/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<WeatherService>();
 builder.Services.AddHttpClient<WeatherService>();
 
-var encKey = builder.Configuration["OPENWEATHER_KEY"];
+var encKey = builder.Configuration["OpenWeather:key"];
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
